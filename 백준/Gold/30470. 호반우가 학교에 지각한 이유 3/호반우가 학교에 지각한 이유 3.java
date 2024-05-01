106eda00ff4337b3e6c278fd2293ba4fde54b9f9
jseo_k <thswltjr11@gmail.com>
2024-05-01 13:06:43 +0900

    [Gold V] Title: 호반우가 학교에 지각한 이유 3, Time: 764 ms, Memory: 138080 KB -BaekjoonHub

--- 백준/Gold/30470. 호반우가 학교에 지각한 이유 3/호반우가 학교에 지각한 이유 3.java
+++ 백준/Gold/30470. 호반우가 학교에 지각한 이유 3/호반우가 학교에 지각한 이유 3.java
@@ -16,13 +16,7 @@ public class Main {
             int m = Integer.parseInt(st.nextToken());
 
             if (oper == 1) {
-                // 스택에 나무가 있으면서 value가 같으면 갯수만 증가
-                // 나무가 항상 큰 값으로 들어오긴 해서.. 이게 맞는지는 애매하네
-                if (!stack.isEmpty() && stack.peek().value == m) {
-                    stack.peek().repeat += 1;
-                } else {
-                    stack.add(new Tree(m, 1));
-                }
+                stack.add(new Tree(m, 1));
                 res += m;
             } else {
                 // 비어있으면 패스
@@ -32,7 +26,7 @@ public class Main {
                 // 마법 수행
                 int newSize = Math.max(stack.peek().value - m, 0);
                 int cnt = 0;
-                Stack<Tree> temp = new Stack<>();
+
                 // 스택의 최상단 길이가 마법 기준보다 큰 경우
                 while (!stack.isEmpty() && stack.peek().value > newSize) {
                     Tree now = stack.pop();
