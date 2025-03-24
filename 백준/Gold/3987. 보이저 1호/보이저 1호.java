9943610c4403d76c0c59fff28a32afe75b3824fa
jseo_k <thswltjr11@gmail.com>
2025-03-24 13:46:46 +0900

    [Gold V] Title: 보이저 1호, Time: 288 ms, Memory: 47612 KB -BaekjoonHub

--- 백준/Gold/3987. 보이저 1호/보이저 1호.java
+++ 백준/Gold/3987. 보이저 1호/보이저 1호.java
@@ -2,15 +2,13 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int n,m;
-    static char[][] board;
-    static int resDir;
-    static int resDepth;
-    // 상하좌우
+    // URDL
     static int[] dx = {0,1,0,-1};
     static int[] dy = {-1,0,1,0};
-    static boolean isVoyager;
+    static int n,m;
+    static char[][] board;
     static boolean[][][] visited;
+    static boolean isVoyager;
     static int bestVoyagerDir = -1;
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -20,83 +18,88 @@ public class Main {
         m = Integer.parseInt(st.nextToken());
         
         board = new char[n][m];
-        for (int i = 0; i < n; i++){
+        for (int i = 0; i<n; i++){
             String line = br.readLine();
-            for (int j = 0; j < m; j++){
+            for (int j = 0; j <m; j++){
                 board[i][j] = line.charAt(j);
             }
         }
         
         st = new StringTokenizer(br.readLine());
-        // 0 인덱스 조정
         int startY = Integer.parseInt(st.nextToken()) - 1;
         int startX = Integer.parseInt(st.nextToken()) - 1;
         
-        // dfs 돌면서 뎁스 측정
-        // 만약 뎁스가 갱신될수있으면 갱신
-        // 같을때는 알파벳 교체
-        // 방문 배열은 방향마다 계속 초기화
+        // --------------
+        
+        // 임시용 큰값
+        int resDir = 5;
+        int resCnt = 0;
+        
+        // 4방향으로 이동
         for (int d = 0; d < 4; d++){
             visited = new boolean[n][m][4];
-            dfs(startX, startY, d, 1);
+            int cnt = run(startX, startY, d);
+            
+            // 더 많이 전파된다면 그거로 교체
+            if (resCnt < cnt) {
+                resDir = d;
+                resCnt = cnt;
+            } else if (resCnt == cnt){
+                // 같다면 더 앞서는 방향으로 교체
+                resDir = Math.min(resDir, d);
+            }
         }
         
-        char[] dirChar = {'U', 'R', 'D', 'L'};
-        System.out.println(dirChar[isVoyager ? bestVoyagerDir : resDir]);
-        System.out.println(isVoyager ? "Voyager" : resDepth);
+        char[] dirChar = {'U','R','D','L'};
+        if (isVoyager){
+            System.out.println(dirChar[bestVoyagerDir]);
+            // 광야로 걸어가~
+            System.out.println("Voyager");
+        } else {
 
+            System.out.println(dirChar[resDir]);
+            System.out.println(resCnt);
+        }
     }
     
-    static void dfs(int x, int y, int dir, int depth){
-        // 시작 지점 백업
-        int sX = x;
-        int sY = y;
-        int sDir = dir;
-        
-        // dfs -> while로 수정
-        // 이유는 재귀 한계
-        while (true){
-            x = x + dx[dir];
-            y = y + dy[dir];
-            // 만약 벽이거나 블랙홀인 경우 -> 중단이 된다는 거니까 갱신하고 종료
-            if (x < 0 || y < 0 || x >= m || y >= n || board[y][x] == 'C') {
-                if (depth > resDepth) {
-                    resDepth = depth;
-                    resDir = sDir;
-                } else if (depth == resDepth) {
-                    resDir = Math.min(resDir, sDir);
-                }
-                return;
+    static int run(int x, int y, int d){
+        int cnt = 0;
+        // 벽 너머가거나 무한루프 돌때까지 반복
+        while (true){        
+            cnt ++;
+            // 방향대로 증가
+            int nx = x + dx[d];
+            int ny = y + dy[d];
+            
+            // 기저조건
+            if (nx < 0 || ny < 0 || nx >= m || ny >= n || board[ny][nx] == 'C') {
+                break;
             }
-
-        
-            // 만약 처음으로 돌아왔는데 방향도 똑같다면 -> 무한반복임
-            if (visited[y][x][dir]) {
-                
+            
+            if (visited[ny][nx][d]) {
                 isVoyager = true;
-                if (bestVoyagerDir == -1 || sDir < bestVoyagerDir) {
-                    bestVoyagerDir = sDir;
+                if (bestVoyagerDir == -1 || bestVoyagerDir > d){
+                    bestVoyagerDir = d;
                 }
-                return;
+                break;
+            }    
+            
+            visited[ny][nx][d] = true;
+            
+            // 만약 행성이라면 방향 전환
+            if (board[ny][nx] == '\\'){
+                d = 4 - d - 1;
+            } else if (board[ny][nx] == '/'){
+                if (d == 0) d = 1;
+                else if(d == 1) d = 0;
+                else if(d == 2) d = 3;
+                else d = 2;
             }
             
-            visited[y][x][dir] = true;
-            
-            // 방향 회전
-            if (board[y][x] == '\\') {
-                if (dir == 0) dir = 3;
-                else if (dir == 2) dir = 1;
-                else if (dir == 1) dir = 2;
-                else dir = 0;
-            } else if (board[y][x] == '/') {
-                if (dir == 0) dir = 1;
-                else if(dir == 1) dir = 0;
-                else if (dir == 2) dir = 3;
-                else dir = 2;
-            }
-            
-            depth++;
+            x = nx;
+            y = ny;
         }
+        
+        return cnt;
     }
-    // 0 1 2 3 : 상하좌우
 }
