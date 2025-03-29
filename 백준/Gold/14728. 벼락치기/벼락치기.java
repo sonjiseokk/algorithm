625a815fe8d5ac6da65cb2217a2a7883a7713c2a
jseo_k <thswltjr11@gmail.com>
2025-03-29 19:15:48 +0900

    [Gold V] Title: 벼락치기, Time: 132 ms, Memory: 18580 KB -BaekjoonHub

--- 백준/Gold/14728. 벼락치기/벼락치기.java
+++ 백준/Gold/14728. 벼락치기/벼락치기.java
@@ -8,13 +8,12 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int t = Integer.parseInt(st.nextToken());
-        
-        Node[] nodes = new Node[n+1];
-        for (int i = 1; i <= n; i++){
+
+        int[][] questions = new int[n][2];
+        for (int i = 0; i < n; i++){
             st = new StringTokenizer(br.readLine());
-            int time = Integer.parseInt(st.nextToken());
-            int score = Integer.parseInt(st.nextToken());
-            nodes[i] = new Node(time, score);
+            questions[i][0] = Integer.parseInt(st.nextToken());
+            questions[i][1] = Integer.parseInt(st.nextToken());
         }
         
         // ------------------
@@ -22,35 +21,23 @@ public class Main {
         int[][] dp = new int[n+1][t+1];
         
         for (int i = 1; i <= n; i++){
-            for (int j = 1; j <= t; j++){
-                // 선택한 노드
-                Node now = nodes[i];
-                // 아직 노드의 시간보다 작은 경우
-                if (now.time > j){
+            int k = questions[i-1][0];
+            int s = questions[i-1][1];
+            for (int j = 0; j <= t; j++){
+                // 배낭에 넣을 수 있는 크기인 경우
+                if (j >= k) {
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i-1][j-k] + s);
+                } else{
+                    // 배낭에 넣을 수 없는 경우
                     dp[i][j] = dp[i-1][j];
-                } else {
-                    // 노드의 시간인 경우
-                    // 해당 노드를 선택하는 것과 선택하지 않은 것중 더 큰것을 선택
-                    dp[i][j] = Math.max(dp[i-1][j], dp[i-1][j - now.time] + now.score);
                 }
             }
         }
         
-        // 마지막줄에서 제일 큰 애 선택
         int res = 0;
         for (int i = 0; i <= t; i++){
-            res = Math.max(res, dp[n][i]);
+            res = Math.max(dp[n][i], res);
         }
-        
         System.out.println(res);
-    }
-    
-    static class Node {
-        int time;
-        int score;
-        public Node(int time, int score){
-            this.time = time;
-            this.score = score;
-        }
     }
 }
